d5d14c615e72b3b71bc61cb0c0ba5ecf8276140c
Ayoub Boumallassa <aboumall@z4r10p4.42lyon.fr>
2024-11-07 15:23:35 +0100

    feat: ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 14:07:25 by aboumall          #+#    #+#             */
+/*   Updated: 2024/11/07 15:18:06 by aboumall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+        size_t  i;
+	unsigned char *c_dest;
+	unsigned char *c_src;
+
+	c_dest = (unsigned char *)dest;
+	c_src = (unsigned char *)src;
+        if (c_dest < c_src)
+        {
+                i = 0;
+                while(i < n)
+                {
+                        c_dest[i] = c_src[i];
+                        i++;
+                }
+        }
+        else
+        {
+                i = n;
+                while(i > 0)
+                {
+                        c_dest[i - 1] = c_src[i - 1];
+                        i--;
+                }
+        }
+	return (dest);
+}
+
+int     main()
+{
+    char data[] = "OpenAIAI";
+    
+    ft_memmove(data + 3, data, 5);  // Copie "OpenA" dans la zone à partir de "enAI"
+    printf("Avec chevauchement (memmove): %s\n", data); // Affiche "OpeOpenA"
+
+    return 0;
+}
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 11:13:10 by aboumall          #+#    #+#             */
-/*   Updated: 2024/11/06 11:39:51 by aboumall         ###   ########.fr       */
+/*   Updated: 2024/11/07 14:38:46 by aboumall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,31 @@ static size_t ft_strlen(const char *str)
     return (size);
 }
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t i;
-    size_t dst_len;
-    size_t src_len;
+	size_t	i;
+	size_t	dest_size;
+	size_t	src_size;
 
-    dst_len = ft_strlen(dst);
-    src_len = ft_strlen(src);
-    i = 0;
-    while(src[i] && size > i)
-    {
-        dst[i + dst_len] = src[i];
-        i++;
-    }
-    dst[i + dst_len] = '\0';
-    return (dst_len + src_len);
+	i = 0;
+	dest_size = 0;
+	src_size = 0;
+	while (dst[dest_size])
+		dest_size++;
+	while (src[src_size])
+		src_size++;
+	if (size == 0 || size < dest_size)
+		return (src_size + size);
+	while (src[i] && (dest_size + i) < size - 1)
+	{
+		dst[dest_size + i] = src[i];
+		i++;
+	}
+	dst[dest_size + i] = '\0';
+	return (dest_size + src_size);
 }
 
-int main(void)
+int     main(void)
 {
     char dest[20] = "test ";
     char dest1[20] = "test ";
