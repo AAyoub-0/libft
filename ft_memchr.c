911a4576d943de723a46746421330cdf47032cec
Ayoub Boumallassa <aboumall@z4r10p4.42lyon.fr>
2024-11-07 15:27:20 +0100

    feat: ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:57:37 by aboumall          #+#    #+#             */
-/*   Updated: 2024/11/06 15:41:30 by aboumall         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:26:28 by aboumall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@ void    *ft_memchr(const void *s, int c, size_t n)
         size_t  i;
         unsigned char*  str;
 
-	// i = ft_strlen(s);
-	// if (c == 0)
-	// 	return (&s[i]);
         i = 0;
         str = (unsigned char *)s;
 	while (str[i])
