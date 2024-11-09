ba99104c69d7298f1a037fa95880d54b588977ab
AAyoub-El-Boss <yousalama5@gmail.com>
2024-11-10 00:04:12 +0100

    fix: memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aayoub <aayoub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:57:37 by aboumall          #+#    #+#             */
-/*   Updated: 2024/11/08 13:21:09 by aboumall         ###   ########.fr       */
+/*   Updated: 2024/11/10 00:00:27 by aayoub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,32 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	i = 0;
 	str = (unsigned char *)s;
-	while (str[i] && i < n)
+	while (i < n)
 	{
-		while (str[i] == (unsigned char)c)
+		if (str[i] == (unsigned char)c)
 			return (&str[i]);
 		i++;
 	}
-	return ('\0');
+	return ((void *)0);
 }
 
-/* int     main(void)
-{
-		const char buffer[] = "Bonjour tout le monde";
-		char c = '\0';
-		size_t n = strlen(buffer);
+// int     main(void)
+// {
+// 		char buffer[] = {0, 1, 2 ,3 ,4 ,5};
+// 		char c = 2 + 256;
+// 		size_t n = 3;
 
-		char *result = ft_memchr(buffer, c, n);
-		char *result2 = memchr(buffer, c, n);
+// 		printf("lenght : $%zu<\n", n);
+// 		char *result = ft_memchr(buffer, c, n);
+// 		char *result2 = memchr(buffer, c, n);
 
-		if (result != '\0' && result2 != '\0') {
-				printf("ft_memchr : Caractère '%c' trouvé à la position : %ld,
-					$%s<\n", c, result - buffer, result);
-				printf("\nmemchr : Caractère '%c' trouvé à la position : %ld,
-					$%s<\n", c, result2 - buffer, result2);
-		} else {
-				printf("ft_memchr : Caractère '%c' non trouvé dans les
-					%zu premiers octets. $%s<\n", c, n, result);
-				printf("memchr : Caractère '%c' non trouvé dans les
-					%zu premiers octets. $%s<", c, n, result2);
-		}
+// 		if (result == buffer + 2) {
+// 				printf("ft_memchr : Caractère '%c' trouvé à la position : %ld, $%s<\n", c, result - buffer, result);
+// 				printf("\nmemchr : Caractère '%c' trouvé à la position : %ld, $%s<\n", c, result2 - buffer, result2);
+// 		} else {
+// 				printf("ft_memchr : Caractère '%c' non trouvé dans les %zu premiers octets. $%s<\n", c, n, result);
+// 				printf("memchr : Caractère '%c' non trouvé dans les %zu premiers octets. $%s<\n", c, n, result2);
+// 		}
 
-		return (0);
-} */
+// 		return (0);
+// }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aayoub <aayoub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:28:47 by aboumall          #+#    #+#             */
-/*   Updated: 2024/11/08 17:51:03 by aboumall         ###   ########.fr       */
+/*   Updated: 2024/11/09 23:10:17 by aayoub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,20 +27,20 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	return (ft_strlen(src));
 }
 
-int main(void)
-{
-	char dest[] = "A";
-	char dest1[] = "A";
-	char src[] = "hello !";
-	char src1[] = "hello !";
-	int sizeeeee = 20;
+// int main(void)
+// {
+// 	char dest[] = "A";
+// 	char dest1[] = "A";
+// 	char src[] = "hello !";
+// 	char src1[] = "hello !";
+// 	int sizeeeee = 20;
 
-	size_t size = strlcpy(dest, src, sizeeeee);
-	size_t size1 = ft_strlcpy(dest, src, sizeeeee);
-	printf("strlcpy : return : %u, dest : %s", sizeeeee, dest);
-	printf("\nft_strcpy : return : %u, dest : %s", sizeeeee, dest);
-        if (size == size1)
-                puts("\nTEST SUCCESS");
-        else
-                puts("\nTEST FAILED");
-}
+// 	size_t size = strlcpy(dest, src, sizeeeee);
+// 	size_t size1 = ft_strlcpy(dest, src, sizeeeee);
+// 	printf("strlcpy : return : %u, dest : %s", sizeeeee, dest);
+// 	printf("\nft_strcpy : return : %u, dest : %s", sizeeeee, dest);
+//         if (size == size1)
+//                 puts("\nTEST SUCCESS");
+//         else
+//                 puts("\nTEST FAILED");
+// }
