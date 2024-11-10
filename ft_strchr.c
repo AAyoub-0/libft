727fbdfbd70d08708a8fb89bb52e085237b94ed7
AAyoub-El-Boss <yousalama5@gmail.com>
2024-11-10 02:05:27 +0100

    fix: strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aayoub <aayoub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 12:52:35 by aboumall          #+#    #+#             */
-/*   Updated: 2024/11/08 17:19:54 by aboumall         ###   ########.fr       */
+/*   Updated: 2024/11/10 02:05:02 by aayoub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,24 @@ char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
-	i = ft_strlen(s);
-	if (c == 0)
-		return ((char *)&s[i]);
 	i = 0;
+	c = (unsigned char)c;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if ((unsigned char)s[i] == c)
 			return ((char *)&s[i]);
 		i++;
 	}
+	if (c == '\0')
+		return ((char *)&s[i]);
 	return ((char *)0);
 }
 
-/* int     main(void)
-{
-		char    str[] = "Hello world !";
-		char    c = '\0';
+// int     main(void)
+// {
+// 		char str[] = "teste";
+// 		char    c = 1024;
 
-		printf("ft_strchr : $%s<", ft_strchr(str, c));
-		printf("\nstrchr : $%s<", strchr(str, c));
-} */
+// 		printf("ft_strchr : $%s<", ft_strchr(str, c));
+// 		printf("\nstrchr : $%s<\n", strchr(str, c));
+// }
