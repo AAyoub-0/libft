d5cae0dffffa4d37e4c9f0d19c48cf9245074659
AAyoub-El-Boss <yousalama5@gmail.com>
2024-11-22 22:19:27 +0100

    fix: strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aayoub <aayoub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:45:56 by aboumall          #+#    #+#             */
-/*   Updated: 2024/11/22 21:50:34 by aayoub           ###   ########.fr       */
+/*   Updated: 2024/11/22 22:16:31 by aayoub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	while (((unsigned char)s1[i] == (unsigned char)s2[i]) && (s1[i] && s2[i])
-		&& (i < n))
+		&& (i < n - 1))
 		i++;
 	return (((unsigned char)s1[i] - (unsigned char)s2[i]));
 }
